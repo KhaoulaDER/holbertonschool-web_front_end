4c5a04653d322ece8c83e50f7337f8e37127e7d4
KhaoulaDER <58639852+KhaoulaDER@users.noreply.github.com>
2021-05-24 15:34:20 +0100

    Delete Integral directory

--- Integral/Rectangle.h
+++
@@ -1,4 +0,0 @@
-#ifndef RECTANGLE_H_ /* Include guard */
-#define RECTANGLE_H_
-double rectangleMethod(double a, double b, int n);
-#endif // RECTANGLE_H_
--- Integral/Simpson.h
+++
@@ -1,4 +0,0 @@
-#ifndef SIMPSON_H_ /* Include guard */
-#define SIMPSON_H_
-double simpson(double a, double b, int n);
-#endif // SIMPSON_H_
